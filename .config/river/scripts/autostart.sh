44aaf1b4c640436dd555c92887f7cfbd8cd3421c
januarpancaran <januar352@gmail.com>
2025-04-18 21:37:56 +0700

    add: adding swayidle

--- .config/river/scripts/autostart.sh
+++ .config/river/scripts/autostart.sh
@@ -5,9 +5,10 @@ way-displays >/tmp/way-displays.${XDG_VTNR}.${USER}.log 2>&1 &
 PROGRAMS=(
   i3bar-river
   swww-daemon
-  swayidle
   /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 )
+
+monitor=$(wlr-randr | grep DP | awk '{print $1}')
 
 set_gnome() {
   gsettings set org.gnome.desktop.interface "$1" "$2"
@@ -17,6 +18,23 @@ start() {
   for program in ${PROGRAMS[@]}; do
     riverctl spawn $program &
   done
+
+  swayidle -w \
+    timeout 150 'brightnessctl -s set 10' \
+    resume 'brightnessctl -r' \
+    \
+    timeout 150 'brightnessctl -sd rgb:kbd_backlight set 0' \
+    resume 'brightnessctl -rd rgb:kbd_backlight' \
+    \
+    timeout 300 'swaylock' \
+    \
+    timeout 330 'wlopm --off $monitor' \
+    resume 'wlopm --on $monitor' \
+    \
+    timeout 600 'systemctl suspend' \
+    \
+    before-sleep 'swaylock' \
+    lock 'swaylock' &
 
   set_gnome gtk-theme "Dracula"
   set_gnome icon-theme "Dracula"
