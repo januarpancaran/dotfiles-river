ef87b7faeb3331d4fac3d5dbe52203900ce609d5
januarpancaran <januar352@gmail.com>
2025-04-23 05:50:30 +0700

    add: adding screenshot binding

--- .config/river/scripts/appbinding.sh
+++ .config/river/scripts/appbinding.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 map() {
-  key="$1"
-  shift
-  riverctl map normal Super "$key" spawn "$@"
+	key="$1"
+	shift
+	riverctl map normal Super "$key" spawn "$@"
 }
 
 map_shift() {
-  key="$1"
-  shift
-  riverctl map normal Super+Shift "$key" spawn "$@"
+	key="$1"
+	shift
+	riverctl map normal Super+Shift "$key" spawn "$@"
 }
 
 map T ghostty
@@ -21,3 +21,10 @@ map D "discord --ozone-platform=wayland"
 map SemiColon spotify-launcher
 map O obs
 map M wlogout
+
+map_shift N "_JAVA_AWT_WM_NONREPARENTING=1 netbeans"
+
+# Screenshot
+map_shift S 'grim -g "$(slurp)" ~/Pictures/Screenshots/Screenshot_$(date +'%Y%m%d_%H%M%S').png && notify-send "Screenshot Taken!" -t 2000'
+map_shift P 'grim ~/Pictures/Screenshots/Screenshot_$(date +'%Y%m%d_%H%M%S').png && notify-send "Screenshot Taken!" -t 2000'
+map P 'grim - | wl-copy && notify-send "Screenshot Copied to Clipboard" -t 2000'
