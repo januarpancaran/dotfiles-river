57b1ed488faea43b5c80bf6a29bbb5bdb6b95db2
januarpancaran <januar352@gmail.com>
2025-04-25 20:14:26 +0700

    add: adding dunst

--- .config/river/scripts/media.sh
+++ .config/river/scripts/media.sh
@@ -5,9 +5,9 @@ for mode in normal locked; do
   riverctl map $mode None XF86Eject spawn 'eject -T'
 
   # Control pulse audio volume with pamixer (https://github.com/cdemoulins/pamixer)
-  riverctl map $mode None XF86AudioRaiseVolume spawn 'wpctl set-volume -l 2 @DEFAULT_AUDIO_SINK@ 5%+'
-  riverctl map $mode None XF86AudioLowerVolume spawn 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-'
-  riverctl map $mode None XF86AudioMute spawn 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'
+  riverctl map $mode None XF86AudioRaiseVolume spawn '~/.local/bin/changevolume up'
+  riverctl map $mode None XF86AudioLowerVolume spawn '~/.local/bin/changevolume down'
+  riverctl map $mode None XF86AudioMute spawn '~/.local/bin/changevolume toggle'
 
   # Control MPRIS aware media players with playerctl (https://github.com/altdesktop/playerctl)
   riverctl map $mode None XF86AudioMedia spawn 'playerctl play-pause'
@@ -16,6 +16,6 @@ for mode in normal locked; do
   riverctl map $mode None XF86AudioNext spawn 'playerctl next'
 
   # Control screen backlight brightness with brightnessctl (https://github.com/Hummer12007/brightnessctl)
-  riverctl map $mode None XF86MonBrightnessUp spawn 'brightnessctl set +5%'
-  riverctl map $mode None XF86MonBrightnessDown spawn 'brightnessctl set 5%-'
+  riverctl map $mode None XF86MonBrightnessUp spawn '~/.local/bin/changebrightness up'
+  riverctl map $mode None XF86MonBrightnessDown spawn '~/.local/bin/changebrightness down'
 done
